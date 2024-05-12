45060bce8047ce7b1b577b3351471b6f482ca19b
rui <925572702@qq.com>
2024-05-12 17:20:51 +0800

    first commit

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+
+export default function Index() {
+  useLoad(() => {
+    console.log("Page loaded.");
+  });
+
+  return (
+    <View className="bg-blue-600">
+      <Text className="text-6xl">Hello world!0000555009</Text>
+    </View>
+  );
+}
